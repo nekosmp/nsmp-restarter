680c77b44bc10a710c6558d4587e715b1cb65eb4
Atakku <atakkudev@gmail.com>
2024-03-14 02:55:07 +0100

    Fixed the hang issues and cleaned up the code

--- java/server/rs/neko/smp/restarter/server/ServerRestarter.java
+++ java/server/rs/neko/smp/restarter/server/ServerRestarter.java
@@ -14,6 +14,22 @@ import net.minecraft.text.Text;
 public class ServerRestarter implements DedicatedServerModInitializer {
   public static final Logger LOGGER = LoggerFactory.getLogger("nsmp-restarter");
   private static final Timer TIMER = new Timer();
+  private static final int[] INTERVALS = new int[] {
+      60 * 30,
+      60 * 15,
+      60 * 10,
+      60 * 5,
+      60 * 2,
+      60,
+      30,
+      15,
+      10,
+      5,
+      4,
+      3,
+      2,
+      1
+  };
   private static final int OFFSET = 6 * 60 * 60 * 1000;
 
   private static void shedule(Runnable runnable, int delay) {
@@ -25,39 +41,28 @@ public class ServerRestarter implements DedicatedServerModInitializer {
     }, delay);
   }
 
-  public static void broadcast(MinecraftServer server, String text) {
-    server.getPlayerManager().broadcast(Text.literal(text), false);
-  }
-
-  private static void announceMin(MinecraftServer s, int min) {
-    shedule(() -> broadcast(s, "Server restart in " + min + " minutes"), OFFSET - min * 60000);
-  }
-
-  private static void announceSec(MinecraftServer s, int sec) {
-    shedule(() -> broadcast(s, "Server restart in " + sec + " seconds"), OFFSET - sec * 1000);
+  private static void announce(MinecraftServer server, int sec) {
+    shedule(() -> {
+      String output = "Server restart in ";
+      if (sec > 60) {
+        output += sec / 60 + " minutes";
+      } else {
+        output += sec + " seconds";
+      }
+      server.getPlayerManager().broadcast(Text.literal(output), false);
+    }, OFFSET - sec * 1000);
   }
 
   @Override
   public void onInitializeServer() {
     LOGGER.info("Initializing NSMP Restarter");
-    ServerLifecycleEvents.SERVER_STARTED.register((server) -> init(server));
-  }
-
-  private void init(MinecraftServer s) {
-    announceMin(s, 30);
-    announceMin(s, 15);
-    announceMin(s, 10);
-    announceMin(s, 5);
-    announceMin(s, 2);
-    announceSec(s, 60);
-    announceSec(s, 30);
-    announceSec(s, 15);
-    announceSec(s, 10);
-    announceSec(s, 5);
-    announceSec(s, 4);
-    announceSec(s, 3);
-    announceSec(s, 2);
-    announceSec(s, 1);
-    shedule(() -> s.stop(false), OFFSET);
+    ServerLifecycleEvents.SERVER_STARTED.register((server) -> {
+      for (int interval : INTERVALS)
+        announce(server, interval);
+      shedule(() -> server.stop(false), OFFSET);
+    });
+    ServerLifecycleEvents.SERVER_STOPPING.register((s) -> {
+      TIMER.cancel();
+    });
   }
 }
